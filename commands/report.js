bd602ad0cf789a5ae6f61dc5cb4fc8e79aff0227
HavenStadRP-Bo <bram.debont.11@outlook.com>
2025-08-17 21:10:44 +0200

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -1,11 +1,30 @@
-const { EmbedBuilder } = require("discord.js");
-const logger = require("../utils/logger");
+// commands/report.js
+import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
+import fs from 'fs';
 
-module.exports = {
-    name: "report",
-    description: "Meld een speler",
-    options: [
-        {
-            name: "gebruiker",
-            type: 6,
-            description: "Wie
+export const data = new SlashCommandBuilder()
+  .setName('report')
+  .setDescription('📢 Maak een report')
+  .addStringOption(opt => opt.setName('jouwnaam').setDescription('Jouw Roblox naam').setRequired(true))
+  .addStringOption(opt => opt.setName('tegen').setDescription('Tegen wie (Roblox of @user)').setRequired(true))
+  .addStringOption(opt => opt.setName('reden').setDescription('Reden').setRequired(true));
+
+export async function execute(interaction) {
+  const jouw = interaction.options.getString('jouwnaam');
+  const tegen = interaction.options.getString('tegen');
+  const reden = interaction.options.getString('reden');
+
+  const channel = interaction.guild.channels.cache.get('1406025829259022387');
+
+  const embed = new EmbedBuilder()
+    .setColor('Orange')
+    .setTitle('📢 Nieuw Report')
+    .addFields(
+      { name: '👤 Reporter', value: jouw, inline: true },
+      { name: '🎯 Tegen', value: tegen, inline: true },
+      { name: '📖 Reden', value: reden, inline: false }
+    );
+
+  await channel.send({ embeds: [embed] });
+  await interaction.reply('✅ Je report is verzonden!');
+}
