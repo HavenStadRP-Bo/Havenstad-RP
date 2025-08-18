b9e629291bd8554014cea754083c18e8b41b393e
HavenStadRP-Bo <bram.debont.11@outlook.com>
2025-08-18 14:43:59 +0200

    Update permissions.js

--- utils/permissions.js
+++ utils/permissions.js
@@ -1,10 +1,12 @@
-module.exports = {
-    isMod: (interaction) => {
-        const modRoles = [
-            "1404873533951312024",
-            "1405102488775823430",
-            "1406025210527879238"
-        ];
-        return interaction.member.roles.cache.some(r => modRoles.includes(r.id));
-    }
-};
+// utils/permissions.js
+export const modRoles = [
+  "1406025210527879238", // Beheer
+  "1406942631522734231", // Manager
+  "1406942944627265536", // Superizer
+  "1406943073694515280", // SR.Mod
+  "1406943251826606234", // MOD
+];
+
+export function hasModPermission(member) {
+  return member.roles.cache.some(role => modRoles.includes(role.id));
+}
